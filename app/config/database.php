65a254e7e187c924c1974477389f798351465259
Alexander Nicholson <alexj.nich@hotmail.com>
2014-09-27 22:58:32 -0500

    Fixed connection to PostGres

--- app/config/database.php
+++ app/config/database.php
@@ -1,5 +1,13 @@
 <?php
 
+
+$url = parse_url(getenv("DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return array(
 
 	/*
@@ -65,10 +73,10 @@ return array(
 
 		'pgsql' => array(
 			'driver'   => 'pgsql',
-			'host'     => 'localhost',
-			'database' => 'forge',
-			'username' => 'forge',
-			'password' => '',
+			'host'     => $host,
+			'database' => $database,
+			'username' => $username,
+			'password' => $password,
 			'charset'  => 'utf8',
 			'prefix'   => '',
 			'schema'   => 'public',
