40d3bd968be02e93137b3c11ef61a1f2b9902f1b
Alexander Nicholson <alexj.nich@hotmail.com>
2014-09-29 20:05:09 -0500

    Attempt 42

--- app/SMS/Gateway.php
+++ app/SMS/Gateway.php
@@ -34,8 +34,8 @@ class Gateway implements SMSGateway {
 								CURLOPT_POST => 1,
 								CURLOPT_POSTFIELDS => array(
 
-									'api_key' => Config::get('Nexmo.NexmoID'),
-									'api_secret' => Config::get('Nexmo.NexmoSecret'),
+									'username' => Config::get('Nexmo.NexmoID'),
+									'password' => Config::get('Nexmo.NexmoSecret'),
 									'text' => $message,
 									'from' => 'B.A.L.E',
 									'to' => $number
