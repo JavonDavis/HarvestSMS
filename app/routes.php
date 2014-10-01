e2599f0bb79607510db739f08aab16946fcd4c11
Javon <javon_davis14@hotmail.com>
2014-10-01 01:45:04 -0500

    log in updates

--- app/routes.php
+++ app/routes.php
@@ -119,9 +119,9 @@ Route::post('/auth',function() {
 	echo 'HERjE';
 	foreach($users as $user)
 	{
-		echo $number.'HERE\n'.$user;
-		echo $user->number.'|'.$number.'|'.$user->number == $number;
-		echo $user->password.'|'.$password.'|'.$user->password == $password;
+		echo $number.'HERE\n'.($user->number);
+		echo ($user->number).'|'.$number.'|'.($user->number == $number);
+		echo ($user->password).'|'.$password.'|'.($user->password == $password);
 		if($user->number == $number && $user->password == $password)
 		{
 			Session::put('user',$user);
