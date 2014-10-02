cc7bf4e9728ec2b426606aa68bdab8860a63279d
Javon <javon_davis14@hotmail.com>
2014-10-02 00:08:27 -0500

    sms fixes

--- app/routes.php
+++ app/routes.php
@@ -92,7 +92,7 @@ Route::get('/msgreply', function(){
 						}
 						elseif(substr($int_version,0, 3) == $crop_prefix)
 						{
-							$sms->reply(substr($int_version,3,4));
+							$sms->reply(substr($int_version,3,3));
 							if(substr($int_version,3,strlen($int_version)-2) == $crop->crop_id)
 							{
 							$lastDigit = substr($int_version, strlen($int_version)-2,strlen($int_version)-1);
