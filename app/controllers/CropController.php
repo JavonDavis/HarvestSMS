25b4b06d8c0d5a20dab1c62b9356f4df037debd9
Alexander Nicholson <alexj.nich@hotmail.com>
2014-10-02 23:34:10 -0500

    post crop

--- app/controllers/CropController.php
+++ app/controllers/CropController.php
@@ -12,7 +12,22 @@ class CropController extends \BaseController {
 
 	public function postCropForm()
 	{
-		dd (Input::get('fertilizers'));
+		$crop = new Crop;
+
+		$crop->name = Input::get('name');
+		$crop->price = Input::get('price');
+		$crop->days_until_harvest = Input::get('days');
+		$crop->amount_produced = Input::get('amount');
+		$crop->crop_id = strval(rand(0, 100));
+
+		$crop->save();
+
+		$fertilizers = Input::get('fertilizers');
+		foreach ($fertilizers as $fertilizer) {
+			$toAttach = Fertilizer::where('name', '=', $fertilizer)->first();
+			$crop->fertilizers->associate($toAttach);
+		}
+
 	}
 
 }
