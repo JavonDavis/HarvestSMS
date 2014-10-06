34203eafc07dd651c15fa0e2ced14b98b80df39c
Javon <javon_davis14@hotmail.com>
2014-10-06 14:25:10 -0500

    sms fixes

--- app/routes.php
+++ app/routes.php
@@ -184,7 +184,7 @@ Route::get('/msgreply', function(){
 		}
 		elseif(substr($int_version,0, 3) == $livestock_prefix)
 		{
-			strlen($int_version) == 4 ? $id = ssubstr($int_version,3,strlen($int_version)-3):ubstr($int_version,3,strlen($int_version)-4);
+			strlen($int_version) == 4 ? $id = substr($int_version,3,strlen($int_version)-3):substr($int_version,3,strlen($int_version)-4);
 			foreach($livestocks as $livestock)
 			{
 				if(substr($int_version,3) == $livestock->id)
