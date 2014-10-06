d5c35839884eaab0cc95453c9efda06276f7782a
Javon <javon_davis14@hotmail.com>
2014-10-06 14:30:24 -0500

    sms fixes

--- app/routes.php
+++ app/routes.php
@@ -121,6 +121,7 @@ Route::get('/msgreply', function(){
 		elseif(substr($int_version,0, 3) == $crop_prefix)
 		{
 			$id = strlen($int_version) == 4 ? substr($int_version,3,strlen($int_version)-3):substr($int_version,3,strlen($int_version)-4);
+			$sms-reply($id);
 			try
 			{
 				$crop = Crop::findOrFail($id);
